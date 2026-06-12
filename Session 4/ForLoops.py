4162f8fd7ab596ab0c0bf13c9023f502d8538d6b
Paras-M2012 <paraspanchal1610@gmail.com>
2026-06-12 15:57:21 +0530

    Python

---
+++ Session 4/ForLoops.py
@@ -0,0 +1,15 @@
+for i in range(10):
+    print(i)
+print("Your Loops Is completed")
+for i in range(3,7):
+    print(i)
+print("Your Loops Is completed")
+for i in range(1,10,4):
+    print(i)
+print("Your Loops Is completed")
+for i in range(10,0,-1):
+    print(i)
+print("Your Loops Is completed")
+
+
+
---
+++ Session 4/Pattern.py
@@ -0,0 +1,58 @@
+'''
+for i in range(1,10):
+    for j in range(i):
+        print("*",end="")
+    print()
+THis is the actual lowest code for print patter
+'''
+
+'''
+for i in range(1,10):
+    print("*"*i)
+'''
+
+
+'''
+for i in range(10,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+'''
+
+'''
+print only opposite pyramid
+for i in range(1,10):
+    print(" "*(9-i),"*"*i)
+'''
+
+'''
+make a full pyramid with put only space before star
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+'''
+
+'''
+pyramid will convert whole
+for i in range(9,0,-1):
+    print(" "*(9-i)," *"*i)
+'''
+
+'''
+make a diamond this code
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+for i in range(9,0,-1):
+    print(" "*(9-i)," *"*i)
+'''
+
+'''
+make this code damru
+'''
+for i in range(9,0,-1):
+    print(" "*(9-i)," *"*i)
+for i in range(1,10):
+    print(" "*(9-i)," *"*i)
+    
+
+    
+
