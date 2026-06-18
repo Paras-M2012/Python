757d03712ff73214c8ccb4d640d6457697f20cd9
Paras-M2012 <paraspanchal1610@gmail.com>
2026-06-18 13:48:30 +0530

    session 5

---
+++ Sesssion 5/Fruit.py
@@ -0,0 +1,32 @@
+
+'''
+fruits = ['Apple', 'Banana', 'Mango', 'Orange']
+for fruit in fruits:
+    if fruit == 'Banana' :
+        continue
+    print(fruit)
+'''
+
+foods = ['Pizza', 'Burger', 'Pasta', 'Sandwich', 'Burger King']
+
+
+'''
+for food in foods:
+    if food == 'Burger King':
+        print('Found Burger King, stopping search.')
+        break
+    print(food)
+'''
+
+
+'''
+playlists = ['Chill Vibes', 'Workout', 'Focus', 'Party']
+
+for playlist in playlists:
+    if playlist == 'Focus':
+        pass
+    else:
+        print(playlist)
+'''
+
+
